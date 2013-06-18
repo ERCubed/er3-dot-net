8e643fdd2dfff51509ded51cdc0f8ba416ea941e
Eric Roberts <eric@ercubed.com>
2013-06-18 11:57:53 -0500

    Initial commit for the site

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Er3DotNet::Application.config.secret_token = 'f25b2a4f0b1d9312fa535be2f60bf081b4fa6278676e922190a72f82f9271248423120e0f387fbf6ce782e4b0d518551c59a672ca64f3828df8101adba59682f'
+Er3DotNet::Application.config.secret_token = 'Z52H767s22Yh24o990zkQNM72wsKNBpTqfEE56Ja1f8AMl6jWES96sY91o9CUUQNxYW4bpnnx2M02Z9tr4mL6046GRmCf39X4MmmcNkCM1OJs66UGRe59guvmjxS3Y0pMrM8093I7LkQk0bJxflvCR'
