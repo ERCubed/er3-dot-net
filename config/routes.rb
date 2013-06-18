c9e457f0572d6497777366afc8c1abfb482130a1
Eric Roberts <eric@ercubed.com>
2013-06-18 13:37:53 -0500

    Some cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Er3DotNet::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  
   devise_for :users
+  
   resources :users
+
+  
+  root :to => "home#index"
+
 end
