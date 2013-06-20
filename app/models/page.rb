763ead9dc12657df23071d78eb7c4e6a9034d37b
Eric Roberts <eric@ercubed.com>
2013-06-20 17:13:01 -0500

    End date greater than today will list "Current"

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,5 +4,14 @@ class Page < ActiveRecord::Base
     @companytasks = Task.where("company_id = ?", id)
     return @companytasks
   end
+  
+  def self.isCurrent(date)
+    if date > DateTime.now
+      c = "Current"
+    else 
+      c = date.strftime("%B %Y")
+    end 
+    return c
+  end
 
 end
